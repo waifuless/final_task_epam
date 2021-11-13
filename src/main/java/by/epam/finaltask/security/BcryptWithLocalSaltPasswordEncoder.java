0ca9969ae8d45a660dbdb5bbb0cafb9f7f1848c7
Vladimir <vova.prusenok@gmail.com>
2021-11-13 19:30:35 +0300

    change local salt enviroment variable name

--- src/main/java/by/epam/finaltask/security/BcryptWithLocalSaltPasswordEncoder.java
+++ src/main/java/by/epam/finaltask/security/BcryptWithLocalSaltPasswordEncoder.java
@@ -6,7 +6,7 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 public class BcryptWithLocalSaltPasswordEncoder implements PasswordEncoder {
 
     private final static int DEFAULT_COST = 6;
-    private final static String LOCAL_SALT = System.getenv("LOCAL_DICTIONARY_SALT");
+    private final static String LOCAL_SALT = System.getenv("LOCAL_AUCTION_SALT");
     private static volatile BcryptWithLocalSaltPasswordEncoder instance;
 
     private BcryptWithLocalSaltPasswordEncoder() {
