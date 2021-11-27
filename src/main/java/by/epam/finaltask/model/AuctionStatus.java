b8a3926fe5f11ba8300d2b3873f5b16f91c515eb
Vladimir <vova.prusenok@gmail.com>
2021-11-27 03:49:35 +0300

    add auction status

--- src/main/java/by/epam/finaltask/model/AuctionStatus.java
+++ src/main/java/by/epam/finaltask/model/AuctionStatus.java
@@ -1,5 +1,5 @@
 package by.epam.finaltask.model;
 
 public enum AuctionStatus {
-    NOT_VERIFIED, APPROVED_BY_ADMIN, RUNNING, SUSPENDED, ENDED
+    NOT_VERIFIED, APPROVED_BY_ADMIN, RUNNING, SUSPENDED, ENDED, DENIED
 }
